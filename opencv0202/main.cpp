e85f8fbbbc348e0fac4b9c987ecd5183d7640857
Chaojun-Fan <eagle.fan9326@gmail.com>
2020-06-21 16:25:54 +0800

    opencv

--- opencv0202/main.cpp
+++ opencv0202/main.cpp
@@ -1,8 +1,42 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <random>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
-int main(int argc, char *argv[])
+using namespace std;
+using namespace cv;
+
+void colorReduce(Mat image, int div);
+
+int main()
 {
-    QCoreApplication a(argc, argv);
+    Mat image = imread("puppy.jpg");
+    colorReduce(image,64);
 
-    return a.exec();
+    namedWindow("Image");
+    imshow("Image",image);
+
+    waitKey(0);
+
+    return 0;
+}
+void colorReduce(Mat image, int div=64)
+{
+    int nl = image.rows;
+    //element number per row
+    int nc = image.cols * image.channels();
+    for(int j=0;j<nl;j++)
+    {
+        //gain j's address
+        uchar* data=image.ptr<uchar>(j);
+
+        for(int i=0; i<nc; i++)
+        {
+            //handle per element
+            data[i] = data[i]/div*div +div/2;
+
+            //handled element
+        }//finished a cloumn
+    }
 }
